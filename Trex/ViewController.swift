2b9eb36f042d40d38dd6a59014211906110ce163
Remus Lazar <remus@lazar.info>
2015-05-25 11:08:14 +0200

    Initial Commit

---
+++ Trex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trex
+//
+//  Created by Remus Lazar on 25.05.15.
+//  Copyright (c) 2015 Remus Lazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
